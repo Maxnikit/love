f0fef86edd7212c591561d8374e88f2d494f1cc4
Maxnikit <max.nikit.03@gmail.com>
2024-02-13 14:38:32 +0300

    1.0

--- src/index.js
+++ src/index.js
@@ -1,2 +1,40 @@
 import "normalize.css";
 import "./style.css";
+
+const noBtn = document.getElementById("noBtn");
+noBtn.addEventListener("mouseover", function () {
+  noBtn.style.left = `${Math.ceil(Math.random() * 90)}%`;
+  noBtn.style.top = `${Math.ceil(Math.random() * 90)}%`;
+  console.log(noBtn.innerHTML);
+});
+noBtn.addEventListener("click", function () {
+  noBtn.innerHTML = "Всё равно Да!";
+  shePressedNo();
+});
+
+const yesBtn = document.getElementById("yesBtn");
+yesBtn.addEventListener("click", function () {
+  shePressedYes();
+});
+
+function shePressedYes() {
+  const header = document.querySelector("h1");
+  header.textContent = "Урааааа!";
+
+  const img = document.querySelector("img");
+  img.src = "success.gif";
+
+  noBtn.style.display = "none";
+  yesBtn.style.display = "none";
+}
+
+function shePressedNo() {
+  const header = document.querySelector("h1");
+  header.textContent = "Ну ты дааа...";
+
+  const img = document.querySelector("img");
+  img.src = "fail.gif";
+
+  noBtn.style.display = "none";
+  yesBtn.style.display = "none";
+}
